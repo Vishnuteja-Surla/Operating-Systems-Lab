756da427bbbdfbe597f202e24c9ef930325a2c55
Vishnuteja-Surla <cs21b2037@iiitdm.ac.in>
2023-09-15 17:16:31 +0530

    Change ones complement method

--- Lab-8/Question-2/receiver.c
+++ Lab-8/Question-2/receiver.c
@@ -13,6 +13,22 @@ Roll: CS21B2037
 #include <sys/stat.h>
 #include <fcntl.h>
 
+// Function to calculate one's complement of an integer 'a'.
+int ones_complement(int a) {
+
+    int b = 0, p = 1;
+    while(a > 0){
+        int k = a%2;
+        if(k==0){
+            b += p;
+        }
+        p *= 2;
+        a /= 2;
+    }
+    return b;
+
+}
+
 int main() {
     int fd1;
     char *myfifo = "/tmp/myfile.txt";
@@ -30,8 +46,8 @@ int main() {
 
     close(fd1);
 
-    // Calculate the one's complement of 'a'.
-    b = ~a;
+    // Calculate the one's complement of 'a' using the ones_complement function.
+    b = ones_complement(a);
 
     // Reopen the named pipe for writing.
     fd1 = open(myfifo, O_WRONLY);
