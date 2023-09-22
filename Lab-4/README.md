22161735535e0dfe9fd60c24ba45e41ed37ad0ab
Vishnuteja-Surla <cs21b2037@iiitdm.ac.in>
2023-09-22 16:32:00 +0530

    Complete Lab-4

---
+++ Lab-4/README.md
@@ -0,0 +1,32 @@
+# Question-1
+
+## Implement a preemptive priority scheduling approach.
+
+(i) Provide the input as P_id, arrival time (all are arriving at a different time), 
+priority of each process, and burst time.
+
+(ii) Show the Completion time, turnaround time, waiting time, and response time for each process.
+
+(iii) Compute average turnaround time, waiting time, and response time.
+
+(iv) Print the schedule for the given input.
+
+### Note: You can use only integers for process id. 
+Use the array implementation (dynamically using malloc()) for the Queue. 
+At the end of the program remove the queue from the memory.  
+
+# Question-2
+
+## Write a C program to implement round-robin scheduling.
+
+(i)Take input from the user as P_id, arrival time, burst Time, and time quantum.
+
+(ii) Show the turnaround time, waiting time, and response time for each task.
+
+(iii) Compute average turnaround time, waiting time, and response time.
+
+(iv) Count the number of context switches 
+(excluding the count at the beginning of the first execution process and the end of the last execution process)
+
+(v) Your algorithm should work for both the scenario: 
+(a) all tasks arrive at the same time (b) tasks may arrive at a different time
--- Lab-4/Round-Robin-Implementation.c
+++ Lab-4/Round-Robin-Implementation.c
@@ -3,23 +3,6 @@ Name : S. Vishnu Teja
 Roll : CS21B2037
 */
 
-/*
-Implement a preemptive priority scheduling approach.
-
-(i) Provide the input as P_id, arrival time (all are arriving at a different time), 
-priority of each process, and burst time.
-
-(ii) Show the Completion time, turnaround time, waiting time, and response time for each process.
-
-(iii) Compute average turnaround time, waiting time, and response time.
-
-(iv) Print the schedule for the given input.
-
-Note: You can use only integers for process id. 
-Use the array implementation (dynamically using malloc()) for the Queue. 
-At the end of the program remove the queue from the memory.
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -32,7 +15,6 @@ struct process {
     int pid;
     int arrival_time;
     int burst_time;
-    int priority;
     int completion_time;
     int turn_around_time;
     int waiting_time;
@@ -68,21 +50,6 @@ int isempty(){
         return 1;
     }
     return 0;
-}
-
-
-void sort_queue(struct process *arr)
-{
-    int i, j;
-    for (i = front; i < rear; i++) {
-        for (j = front; j < rear; j++) {
-            if (arr[j].priority > arr[j + 1].priority) {
-                struct process temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
 }
 
 
@@ -131,10 +98,12 @@ void print_avgs(struct process *p, int n){
 
 int main(){
 
-    int n;
+    int n, T;
 
     printf("Enter the number of processes: ");
     scanf("%d", &n);
+    printf("Enter the time quantum: ");
+    scanf("%d", &T);
     struct process *p;
     p = (struct process *)malloc(n * sizeof(struct process));
 
@@ -143,8 +112,6 @@ int main(){
         scanf("%d", &p[i].arrival_time);
         printf("Enter the burst time of process %d: ", i+1);
         scanf("%d", &p[i].burst_time);
-        printf("Enter the priority of process %d: ", i+1);
-        scanf("%d", &p[i].priority);
         p[i].pid = i+1;
         p[i].remaining_time = p[i].burst_time;
         p[i].in_queue = 0;
@@ -152,6 +119,9 @@ int main(){
 
     int time = 0;
     int completed_tasks = 0;
+    int context_switches = 0;
+    int left_over = 0;
+    struct process temp;
 
     struct process *ready = (struct process *)malloc(MAX * sizeof(struct process));
 
@@ -160,34 +130,50 @@ int main(){
             if(p[i].in_queue == 0 && p[i].arrival_time <= time){
                 enqueue(ready, p[i]);
                 p[i].in_queue = 1;
-                sort_queue(ready);
             }
+        }
+        if(left_over){
+            enqueue(ready, temp);
+            left_over = 0;
         }
         if(isempty()){
             time++;
             continue;
         }
-        if(ready[front].remaining_time == 0){
+        if(ready[front].remaining_time > T){
+            if(ready[front].remaining_time == ready[front].burst_time){
+                ready[front].response_time = time - ready[front].arrival_time;
+                int pos = find_process(ready[front], p, n);
+                p[pos].response_time = ready[front].response_time;
+            }
+            time = time + T;
+            ready[front].remaining_time = ready[front].remaining_time - T;
+            left_over = 1;
+            temp = ready[front];
+            dequeue(ready);
+            context_switches++;
+        }
+        else{
+            if(ready[front].remaining_time == ready[front].burst_time){
+                ready[front].response_time = time - ready[front].arrival_time;
+                int pos = find_process(ready[front], p, n);
+                p[pos].response_time = ready[front].response_time;
+            }
+            time = time + ready[front].remaining_time;
+            ready[front].remaining_time = 0;
             ready[front].completion_time = time;
             int pos = find_process(ready[front], p, n);
             p[pos].completion_time = ready[front].completion_time;
-            dequeue(ready);
             completed_tasks++;
-        }
-        else{
-        if(ready[front].remaining_time == ready[front].burst_time){
-            ready[front].response_time = time - ready[front].arrival_time;
-            int pos = find_process(ready[front], p, n);
-            p[pos].response_time = ready[front].response_time;
-        }
-        ready[front].remaining_time--;
-        time++;
+            context_switches++;
+            dequeue(ready);
         }
     }
-
+    context_switches--;
     compute_values(p, n);
     printlist(p, n);
     print_avgs(p, n);
+    printf("Number of Context Switches = %d\n", context_switches);
 
     return(0);
 }
