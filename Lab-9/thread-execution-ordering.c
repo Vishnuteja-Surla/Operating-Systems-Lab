72766b57913fc77180fc4db35d0c811fefd60fa1
Vishnuteja-Surla <cs21b2037@iiitdm.ac.in>
2023-10-13 16:45:03 +0530

    Using Semaphores

--- Lab-9/thread-execution-ordering.c
+++ Lab-9/thread-execution-ordering.c
@@ -5,63 +5,46 @@ Roll: CS21B2037
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
-#include<time.h>
 #include<pthread.h>
+#include<semaphore.h>
+
+sem_t s1, s2;
 
 
 void *thread_A_func(void *arg){
 
-    int *t = (int *)arg;
-
+    sem_wait(&s1);
     printf("Statement-A1\n");
-    if(*t==0){
-        sleep(1);
-    }
-    else{
-        sleep(2);
-    } 
+    sem_post(&s2);
+    sem_wait(&s1);
     printf("Statement-A2\n");
+    sem_post(&s2);
 
     pthread_exit(NULL);
 }
 
 void *thread_B_func(void *arg){
 
-    int *t = (int *)arg;
-
+    sem_wait(&s2);
     printf("Statement-B1\n");
-    if(*t==0){
-        sleep(2);
-    }
-    else{
-        sleep(1);
-    } 
+    sem_post(&s1);
+    sem_wait(&s2);
     printf("Statement-B2\n");
+    sem_post(&s1);
 
     pthread_exit(NULL);
 }
 
 
 int main(){
-    
-    srand(time(NULL));
+
+    sem_init(&s1, 0, 1);
+    sem_init(&s2, 0, 1);
 
     pthread_t thread_A, thread_B;
 
-    int r = rand()%2;
-
-    if(r==0){
-        int t = rand()%2;
-        pthread_create(&thread_A, NULL, thread_A_func, (void *)&t);
-        pthread_create(&thread_B, NULL, thread_B_func, (void *)&t);
-    }
-    else{
-        int t = rand()%2;
-        pthread_create(&thread_B, NULL, thread_B_func, (void *)&t);
-        pthread_create(&thread_A, NULL, thread_A_func, (void *)&t);
-    }
-    
+    pthread_create(&thread_A, NULL, thread_A_func, NULL);
+    pthread_create(&thread_B, NULL, thread_B_func, NULL);
 
     pthread_join(thread_A, NULL);
     pthread_join(thread_B, NULL);
